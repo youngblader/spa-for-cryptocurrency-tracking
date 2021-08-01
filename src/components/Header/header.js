40dc795a98e7e6413bf972dbf5ed93ea1ab26302
Wrestling341 <evgeniyzorich@mail.ru>
2021-08-01 12:23:57 +0300

    feat: add component and refactoring

---
+++ src/components/Header/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+  return (
+    <nav className="navbar fixed-top navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">SPA</a>
+        <div className="container-fluid__content">
+          <p className="container-fluid__content-title">Мой портфель</p>
+          <span className="container-fluid__content-subtitle">Информация о портфеле</span>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export { Header };
