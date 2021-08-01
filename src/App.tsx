be338d4106de214b0e08029c641afb73a48f6fee
Wrestling341 <evgeniyzorich@mail.ru>
2021-08-01 21:05:03 +0300

    feat: add redux-store, refactoring App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { Header } from './components/Header/header'
+import React, { FC } from 'react';
+import { Header, Main } from './components';
 
-const App = () => {
+const App: FC = () => {
   return (
     <div className="App">
       <Header/>
-      <div className="container">
-        <div className="row">
-          <div className="col xs">
-            Column
-          </div>
-          <div className="col">
-            Column
-          </div>
-          <div className="col">
-            Column
-          </div>
-        </div>
-      </div>
+      <Main/>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import App from './App';
+import rootReducer from './reducers';
 
 import './index.css';
-import App from './App';
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger)
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
