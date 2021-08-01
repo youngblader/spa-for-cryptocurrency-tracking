6a16214a4acdb20ab53303b864728619403ac413
Wrestling341 <evgeniyzorich@mail.ru>
2021-08-01 22:29:21 +0300

    feat: refactoring type

--- src/components/Card/card.tsx
+++ src/components/Card/card.tsx
@@ -3,19 +3,33 @@ import './card.scss';
 
 type TCryptoCardProps = {
   name: string,
+  marketCapUsd: string,
+  changePercent: string,
+  price: string,
 }
 
 const Card: FC<TCryptoCardProps> = (props) => {
 
   return (
-    <div className="card">
-      <div className="row">
+    <>
+      <div className="row row-cols-4">
         <div className="col">
           <p>{props.name}</p>
-          <button type="button" className="btn btn-primary">Add</button>
         </div>
+        <div className="col">
+          <p className="col-text">MKT CAP {parseFloat(props.marketCapUsd).toFixed(3)}</p>
+        </div>
+        <div className="col">
+        <p className="col-text">TOTAL {parseFloat(props.price).toFixed(3)}</p>
+        </div>
+        <div className="col">
+        <p className="col-text">CHP % {parseFloat(props.changePercent).toFixed(2)}</p>
+        </div>
+        {/* <div className="col-content">
+          <button type="button" className="btn btn-primary">Add</button>
+        </div> */}
       </div>
-    </div>
+    </>
   )
 }
 
