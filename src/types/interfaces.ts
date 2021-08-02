88c28c0d9a9a220813365110eb7ca5c3bceeb96b
Wrestling341 <evgeniyzorich@mail.ru>
2021-08-02 22:02:56 +0300

    add: add new types, interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -5,3 +5,11 @@ export interface ICrypto {
 export interface IStore {
   main: ICrypto;
 }
+
+export interface ISelectedCrypto {
+  selectedCrypto: [];
+}
+
+export interface IStoreCard {
+  card: ISelectedCrypto;
+}
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,13 @@
-import { ICrypto } from "./interfaces";
+import { ICrypto, ISelectedCrypto } from "./interfaces";
 
 export enum actionTypes {
   GET_CRYPTO = 'GET_CRYPTO',
+  SELECTED_CRYPTO = 'SELECTED_CRYPTO'
+}
+
+export type TSelectedCrypto = {
+  type: actionTypes.SELECTED_CRYPTO,
+  payload: [],
 }
 
 export type TGetCryptoAction = {
@@ -9,6 +15,7 @@ export type TGetCryptoAction = {
   payload: [],
 }
 
-export type TCryptoActions = TGetCryptoAction;
+export type TCryptoActions = TGetCryptoAction | TSelectedCrypto;
 
 export type TGetCryptos = (crypto: ICrypto) => TGetCryptoAction;
+export type TSelectedCryptoItem = (selectedCrypto: ISelectedCrypto) => TSelectedCrypto;
