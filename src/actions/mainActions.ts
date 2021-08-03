62b89ee58a661f212894f77be28c34bc26a1ea9c
Wrestling341 <evgeniyzorich@mail.ru>
2021-08-03 21:39:01 +0300

    feat: refactoring components

--- src/actions/mainActions.ts
+++ src/actions/mainActions.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { actionTypes } from '../types/types';
 
 export const getCrypto = async (dispatch: any) => {
-
-  const response = await axios.get('https://api.coincap.io/v2/assets')
+  
+  await axios.get('https://api.coincap.io/v2/assets')
   .then(res => dispatch({
     type: actionTypes.GET_CRYPTO,
     payload: res.data.data
--- src/reducers/currentCryptoCardReducer.ts
+++ src/reducers/currentCryptoCardReducer.ts
@@ -1,17 +1,24 @@
-import { actionTypes, TAddCrypto } from "../types/types";
+import { actionTypes, TCryptoActions } from "../types/types";
 import { ICurrentCrypto } from "../types/interfaces";
 
 const initialState: ICurrentCrypto = {
   wallet: [],
 };
 
-type TcurrentCryptoCardReducer = (state: ICurrentCrypto, action: TAddCrypto) => ICurrentCrypto;
+type TcurrentCryptoCardReducer = (state: ICurrentCrypto, action: TCryptoActions) => ICurrentCrypto;
 
 const currentCryptoCardReducer: TcurrentCryptoCardReducer = (state = initialState, action) => {
   
   switch (action.type) {
 
     case actionTypes.ADD_CRYPTO_TO_WALLET: {
+      return {
+        ...state,
+        wallet: action.payload
+      }
+    }
+
+    case actionTypes.DELETE_CRYPTO: {
       return {
         ...state,
         wallet: action.payload
