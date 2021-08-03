a46bc6c8e96b97a700e4f9b7274294fc8b0d6e5d
Wrestling341 <evgeniyzorich@mail.ru>
2021-08-03 21:37:59 +0300

    add: add modals to project

---
+++ src/components/ModalInputCrypto/modalInputCrypto.tsx
@@ -0,0 +1,65 @@
+import React, { FC, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { IStoreWallet } from '../../types/interfaces';
+import { actionTypes } from '../../types/types';
+
+import './modalInputCrypto.scss';
+
+type TModalInputCryptoProps = {
+  item: any,
+  name: string,
+  symbol: string,
+}
+
+const ModalInputCrypto: FC<TModalInputCryptoProps> = (props) => {
+
+  const dispatch = useDispatch();
+  const [quantityCrypto, setQuantityCrypto] = useState<string>('');
+  const wallet = useSelector((state: IStoreWallet) => state.current.wallet);
+  
+  const onChangeFieldValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuantityCrypto(event.target.value);
+  }
+
+  const addCryptoCurrency = (item: []) => {
+    dispatch({
+      type: actionTypes.ADD_CRYPTO_TO_WALLET,
+      payload: [...wallet, {...item, quantity: quantityCrypto}]
+    });
+    localStorage.setItem('cryptoCurrency', JSON.stringify([...wallet, item]));
+  }
+
+  return (
+    <>
+      <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Buy cryptocurrency</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <div className="modal-body__content"> 
+                <h2>{props.name}</h2>
+                <span className="modal-body__content-text">{props.symbol}</span>
+              </div>             
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="recipient-name" className="col-form-label">Enter the desired quantity:</label>
+                  <input type="text" className="form-control" id="recipient-name" onChange={(event) => onChangeFieldValue(event)} value={quantityCrypto}></input>
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary" onClick={() => addCryptoCurrency(props.item)}>Proceed</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export { ModalInputCrypto };
---
+++ src/components/ModalWindowWallet/modalWindowWallet.tsx
@@ -0,0 +1,76 @@
+import React, { FC, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { IStoreWallet } from '../../types/interfaces';
+import { WalletCard } from '../../components';
+import { actionTypes } from '../../types/types';
+
+const ModalWindowWallet: FC = () => {
+  
+  const dispatch = useDispatch();
+
+  const wallet = useSelector((state: IStoreWallet) => state.current.wallet);
+  console.log(wallet);
+
+  useEffect(() => {
+    const cryptoCurrency: any = localStorage.getItem('cryptoCurrency');
+
+    if(cryptoCurrency !== []) {
+      const parseCryptoCurrency = JSON.parse(cryptoCurrency);
+      if(parseCryptoCurrency?.length) {
+        
+        dispatch({
+          type: actionTypes.ADD_CRYPTO_TO_WALLET,
+          payload: [...parseCryptoCurrency],
+        });
+      }
+    }
+    
+  }, [dispatch]);
+
+  useEffect(() => {
+    localStorage.setItem('cryptoCurrency', JSON.stringify(wallet));
+  }, [wallet]);
+
+  return (
+    <div className="container">
+      <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="staticBackdropLabel">Информация о вашем портфеле</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                {wallet?.length ? (
+                  <>
+                    {wallet.map((item: { id: string; item: any; name: string; priceUsd: string; quantity: string; symbol: string; }, index) => {
+                      return (
+                        <div key={item.id} className="card w-100"> 
+                          <WalletCard
+                            index={index}
+                            id={item.id}
+                            item={item}
+                            name={item.name}
+                            price={item.priceUsd}
+                            quantity={item.quantity}
+                            symbol={item.symbol}
+                          /> 
+                        </div>                    
+                      )
+                    })} 
+                  </>
+                ) : (
+                  <span>У вас нет криптовалюты</span> 
+                )}
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export { ModalWindowWallet };
